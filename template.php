d9f579ff66fd2a77941acf242c05d28fb8ea6f6c
pedromoraes <pedromoraes@gmail.com>
2019-04-27 23:42:28 +0000

    initial

---
+++ template.php
@@ -0,0 +1,10 @@
+<div class="pcc-link-preview">
+	<a href="<?php echo $atts['url']; ?>">
+		<div>
+			<span class="link">🔗 <?php echo $atts['url']; ?></span>
+			<div class="title"><?php echo $atts['title']; ?></div>
+			<img src="<?php echo $atts['image']; ?>">
+			<p><?php echo $content ?></p>
+		</div>
+	</a>
+</div>
