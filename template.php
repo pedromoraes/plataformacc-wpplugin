8e38430d43226966bb1f60bd714db46a8267b0ef
pedromoraes <pedromoraes@gmail.com>
2019-05-02 16:15:08 +0000

    cute-link

--- template.php
+++ template.php
@@ -1,7 +1,7 @@
 <div class="pcc-link-preview">
 	<a href="<?php echo $atts['url']; ?>">
 		<div>
-			<span class="link">Link: <?php echo $atts['url']; ?></span>
+			<span class="link">🔗 <?php echo $atts['url']; ?></span>
 			<div class="title"><?php echo $atts['title']; ?></div>
 			<img src="<?php echo $atts['image']; ?>">
 			<p><?php echo $content ?></p>
