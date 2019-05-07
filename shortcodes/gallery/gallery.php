5741fef63f12712e72dada174b4ca68912ac0180
pedromoraes <pedromoraes@gmail.com>
2019-05-07 01:26:38 +0000

    autolinks on gallery captions

--- shortcodes/gallery/gallery.php
+++ shortcodes/gallery/gallery.php
@@ -1,9 +1,17 @@
 <?php
+function pcc_gallery_autolink( $text ) {
+	$reg_exUrl = "/(http|https|ftp|ftps)\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,3}(\/\S*)?/";
+	if ( preg_match( $reg_exUrl, $text, $url ) )
+		return preg_replace($reg_exUrl, "<a href=\"{$url[0]}\">{$url[0]}</a>", $text);
+	return $text;
+}
+
 function pcc_gallery_shortcode( $atts, $content )
 {
 	$images = get_attached_media( 'image' );
 	ob_start();
 	if ( $content ) {
+		$content = pcc_gallery_autolink( $content );
 		echo "<p>$content</p>";
 	}
 	foreach ( $images as $img ) {
